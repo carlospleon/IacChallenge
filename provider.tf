255c72a345a42bff1fc901c095d6d092074505ab
Carlos Parada <copleon95@gmail.com>
2022-03-29 18:25:26 -0500

    Changing provider.tf

--- provider.tf
+++ provider.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.7"
+      version = "4.8.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
